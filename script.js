6fb3083d0afaa43d43ecf16219531d4da2cd2700
DevJoaoSaltarelli4 <jfelipecaetanosaltarelli@example.com>
2025-03-31 18:21:01 -0300

    tarefa do curso

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const modal = document.getElementById("studyModal"); // aqui esta buscando o elemento Id por nome de studyModal
-    const openBtn = document.querySelector(".btn");
-    const closeBtns = document.querySelectorAll(".close");
+    const openBtn = document.querySelector(".btn");  //Está sendo selecionado o primeiro elemento com a classe .btn e armazenado na variável openBtn.
+    const closeBtns = document.querySelectorAll(".close");// Está sendo selecionada uma NodeList com todos os elementos que possuem a classe .close e armazenada na variável closeBtns.
 
     // Abre a modal ao clicar no botão
     openBtn.addEventListener("click", function() {
