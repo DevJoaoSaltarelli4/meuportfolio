34717fa4f0f5df4c7fe226c641e5111eadf83d78
DevJoaoSaltarelli4 <jfelipecaetanosaltarelli@example.com>
2025-04-09 21:43:06 -0300

    layout calculadora=implementado

--- script.js
+++ script.js
@@ -22,3 +22,65 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+// js calculadora
+
+document.addEventListener("DOMContentLoaded", function () {
+    const botaoCalculadora = document.getElementById("abrirCalculadora");
+
+    botaoCalculadora.addEventListener("click", function () {
+        window.open("calculadora.html", "_blank"); // abre em nova aba
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const resultado = document.querySelector(".resultado");
+    const botoes = document.querySelectorAll(".botao-generico");
+    let expressao = "";
+
+    botoes.forEach(botao => {
+        botao.addEventListener("click", function () {
+            const valor = botao.textContent;
+
+            if (valor === "C") {
+                expressao = "";
+                resultado.textContent = "";
+            } else if (valor === "<") {
+                expressao = expressao.slice(0, -1);
+                resultado.textContent = expressao;
+            } else if (valor === "=") {
+                try {
+                    expressao = expressao.replace(/X/g, "*");
+                    resultado.textContent = eval(expressao);
+                    expressao = resultado.textContent;
+                } catch (error) {
+                    resultado.textContent = "Erro";
+                    expressao = "";
+                }
+            } else {
+                expressao += valor;
+                resultado.textContent = expressao;
+            }
+        });
+    });
+});
+
+const powerButton = document.getElementById('botaoPower');
+const calculadora = document.querySelector('.calculadora');
+
+let ligada = true;
+
+powerButton.addEventListener('click', () => {
+    ligada = !ligada;
+
+    if (ligada) {
+        calculadora.classList.remove('desligada');
+        powerButton.textContent = 'OFF';
+        powerButton.classList.remove('off');
+    } else {
+        calculadora.classList.add('desligada');
+        powerButton.textContent = 'ON';
+        powerButton.classList.add('off');
+    }
+});
+
